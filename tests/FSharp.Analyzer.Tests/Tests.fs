24d3fc93cd16a1de8ec47f77b690264bc6680aad
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2019-03-24 09:45:11 -0400

    Renamed library to FSharp.Analyzer.Core

--- tests/FSharp.Analyzer.Tests/Tests.fs
+++ tests/FSharp.Analyzer.Tests/Tests.fs
@@ -13,31 +13,31 @@ module Result =
 module Tests =
     [<Tests>]
     let tests =
-      testList "samples" [
-        testCaseAsync "Get typechecker results" <| async {
-            let filepath = __SOURCE_DIRECTORY__ + "../../../testData/OptionValue/OptionValue.fsproj"
-            let! results = Checker.parseProject filepath
-            Expecto.Expect.isOk results "Should be ok"
-        }
-        testCaseAsync "Load analyzers" <| async {
-            let dir = __SOURCE_DIRECTORY__ + "../../../packages/analyzers"
-            let analyzers = FsAutoComplete.Analyzers.loadAnalyzers dir
-            Expect.equal 1 (analyzers |> List.length) "Should have loaded one analyzer"
-        }
-        testCaseAsync "Run analyzer" <| async {
+        testList "samples" [
+            testCaseAsync "Get typechecker results" <| async {
+                let filepath = __SOURCE_DIRECTORY__ + "../../../testData/OptionValue/OptionValue.fsproj"
+                let! results = Checker.parseProject filepath
+                Expecto.Expect.isOk results "Should be ok"
+            }
+            testCaseAsync "Load analyzers" <| async {
+                let dir = __SOURCE_DIRECTORY__ + "../../../packages/analyzers"
+                let analyzers = FsAutoComplete.Analyzers.loadAnalyzers dir
+                Expect.equal 1 (analyzers |> List.length) "Should have loaded one analyzer"
+            }
+            testCaseAsync "Run analyzer" <| async {
 
-            let projectPath = __SOURCE_DIRECTORY__ + "../../../testData/OptionValue/OptionValue.fsproj"
-            let! results = Checker.parseProject projectPath
-            printfn "results %A" results
-            let analyzerDir = __SOURCE_DIRECTORY__ + "../../../packages/analyzers"
-            // let analyzers = FsAutoComplete.Analyzers.loadAnalyzers analyzerDir
-            let analyzers = [
-                SampleAnalyzer.optionValueAnalyzer
-            ]
-            printfn "analyzers %A" analyzers
-            let analyzerResults = results |> Result.getOrFail |> Checker.runAnalyzers analyzers
-            printfn "analyzerResults %A" analyzerResults
-            Expect.equal 1 (analyzers |> List.length) "Should have loaded one analyzer"
-        }
+                let projectPath = __SOURCE_DIRECTORY__ + "../../../testData/OptionValue/OptionValue.fsproj"
+                let! results = Checker.parseProject projectPath
+                printfn "results %A" results
+                let analyzerDir = __SOURCE_DIRECTORY__ + "../../../packages/analyzers"
+                // let analyzers = FsAutoComplete.Analyzers.loadAnalyzers analyzerDir
+                let analyzers = [
+                    SampleAnalyzer.optionValueAnalyzer
+                ]
+                printfn "analyzers %A" analyzers
+                let analyzerResults = results |> Result.getOrFail |> Checker.runAnalyzers analyzers
+                printfn "analyzerResults %A" analyzerResults
+                Expect.equal 1 (analyzers |> List.length) "Should have loaded one analyzer"
+            }
 
-      ]
+        ]
